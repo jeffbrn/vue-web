3b4ed37576bae8b74618db60f25323e7038098ee
jeffbrn <jeff.brn@gmail.com>
2023-03-24 17:58:21 -0600

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,73 @@
+<script setup>
+  import { ref } from 'vue'
+
+  defineProps({
+    msg: String,
+  })
+
+  const count = ref(0)
+</script>
+
+<template>
+  <div class="row text-center">
+    <div class="col">
+      <div class="row">
+        <div class="col">
+          <h1 class="fw-bolder py-4">{{ msg }}</h1>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <div class="card mx-auto border border-0" style="width: 26rem">
+            <div class="card-body">
+              <button
+                type="button"
+                class="btn btn-primary mb-4"
+                @click="count++"
+              >
+                count is {{ count }}
+              </button>
+              <p>
+                Edit
+                <code>components/HelloWorld.vue</code> to test HMR
+              </p>
+
+              <p>
+                Check out
+                <a
+                  href="https://vuejs.org/guide/quick-start.html#local"
+                  target="_blank"
+                  >create-vue</a
+                >, the official Vue + Vite starter
+              </p>
+              <p>
+                Install
+                <a
+                  href="https://github.com/vuejs/language-tools"
+                  target="_blank"
+                  >Volar</a
+                >
+                in your IDE for a better DX
+              </p>
+              <p class="text-secondary">
+                Click on the Vite and Vue logos to learn more
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  a {
+    font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;
+  }
+  a:hover {
+    color: #535bf2;
+  }
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li v-for="m in menus" class="nav-item">
+            <a class="nav-link" href="#">{{ m }}</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        menus: ['Home', 'Features', 'Pricing', 'Disabled'],
+      }
+    },
+  }
+</script>
